ca71fffa0f73d476e62a2385e47f978f4dcf636e
Prasanth3121 <109007745+Prasanth3121@users.noreply.github.com>
2022-07-10 07:17:16 +0530

    Add files via upload

---
+++ Mobile.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class Mobile {
+	public static void makeCall() {
+		// TODO Auto-generated method stub
+		System.out.println("Calling");
+	}
+
+	public static void sendMsg() {
+		// TODO Auto-generated method stub
+		System.out.println("Sending Message");
+	}
+
+	public static void main(String[] args) {
+		System.out.println("This is my First Mobile");
+		makeCall();
+		sendMsg();
+		
+	}
+}
