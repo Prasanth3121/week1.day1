91121fca4f0398337a802b94ad893822f2299548
Prasanth3121 <109007745+Prasanth3121@users.noreply.github.com>
2022-07-10 07:22:37 +0530

    Add files via upload

---
+++ Firefox.java
@@ -0,0 +1,12 @@
+package week1.day1;
+
+public class Firefox {
+public static void main(String[] args) {
+	float version = 100.2f;
+	String name = "Firefox";
+	boolean visible = true;
+	int year = 1998;
+	char logo = 'm';
+	System.out.println("This is my First script");
+}
+}
